f33f8bbade58e47c2fbe50f657fa5e3f3b2c4396
cjgo <cltngwn@gmail.com>
2023-06-12 16:57:48 -0500

    good box select

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,36 +28,43 @@
   let selectionRect = {x: 0, y: 0, width: 0, height: 0};
   let isSelecting = false;
 
+  let selectedPoints = [];
+
   onMount(() => {
     d3.select(svg)
       .on('mousedown', function(event) {
-        const [x, y] = d3.pointer(event);
+        const [x, y] = d3.pointer(event, this);
         origin.x = x;
         origin.y = y;
         isSelecting = true;
+        // Reset the previous rectangle selection
+        selectionRect.width = 0;
+        selectionRect.height = 0;
         console.log('mousedown', x, y);
-      })
-      .on('mousemove', function(event) {
-        if (!isSelecting) return;
-        const [x, y] = d3.pointer(event);
-        selectionRect.x = Math.min(origin.x, x);
-        selectionRect.y = Math.min(origin.y, y);
-        selectionRect.width = Math.abs(x - origin.x);
-        selectionRect.height = Math.abs(y - origin.y);
-        console.log('mousemove', x, y);
-      })
-      .on('mouseup', function(event) {
-        if (!isSelecting) return;
-        isSelecting = false;
-        // Compute the list of selected points
-        let selectedPoints = data.filter(d =>
-          xScale(d[x_data]) > selectionRect.x &&
-          xScale(d[x_data]) < selectionRect.x + selectionRect.width &&
-          yScale(d[y_data]) > selectionRect.y &&
-          yScale(d[y_data]) < selectionRect.y + selectionRect.height
-        );
-        console.log('mouseup', selectedPoints);
       });
+
+    window.addEventListener('mousemove', function(event) {
+      if (!isSelecting) return;
+      const [x, y] = d3.pointer(event, svg);
+      selectionRect.x = Math.min(origin.x, x);
+      selectionRect.y = Math.min(origin.y, y);
+      selectionRect.width = Math.abs(x - origin.x);
+      selectionRect.height = Math.abs(y - origin.y);
+      console.log('mousemove', x, y);
+    });
+
+    window.addEventListener('mouseup', function(event) {
+      if (!isSelecting) return;
+      isSelecting = false;
+      // Compute the list of selected points
+      selectedPoints = data.filter(d =>
+        xScale(d[x_data]) > selectionRect.x &&
+        xScale(d[x_data]) < selectionRect.x + selectionRect.width &&
+        yScale(d[y_data]) > selectionRect.y &&
+        yScale(d[y_data]) < selectionRect.y + selectionRect.height
+      );
+      console.log('mouseup', selectedPoints);
+    });
   });
 </script>
 
@@ -70,7 +77,12 @@
 <svg bind:this={svg} width="{width + margin.left + margin.right}" height="{height + margin.top + margin.bottom}">
   <g transform="translate({margin.left}, {margin.top})">
     {#each data as point (point.pc1)}
-      <circle cx="{xScale(point[x_data])}" cy="{yScale(point[y_data])}" r="5" />
+      <circle
+        cx="{xScale(point[x_data])}"
+        cy="{yScale(point[y_data])}"
+        r="5"
+        fill="{selectedPoints.includes(point) ? 'red' : 'black'}"
+      />
     {/each}
     <rect
       x="{selectionRect.x}"
